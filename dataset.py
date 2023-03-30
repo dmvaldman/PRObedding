084aa57bf0cf1b4a15bbb3e59e79ebc7eda329f9
David Valdman <dmvaldman@gmail.com>
2023-03-30 11:15:43 -0700

    Add e5 embedding support

--- dataset.py
+++ dataset.py
@@ -8,22 +8,36 @@ import pandas as pd
 from tenacity import retry, stop_after_attempt
 from datasets import load_dataset
 
+import torch.nn.functional as F
+from torch import Tensor
+from transformers import AutoTokenizer, AutoModel
+
 load_dotenv()
 
-openai_key = os.getenv('OPENAI_API_KEY')
+MAX_CONTENT_LENGTHS = {
+    "openai": 8191,
+    "e5": 1024
+}
 
-openai.api_key = openai_key
+def replace_newline(text):
+    return text.replace('\n', ' ')
 
-MAX_CONTENT_LENGTH = 8191
+def preprocess(texts, model, methods=None):
+    max_content_length = MAX_CONTENT_LENGTHS[model]
+    for i, text in enumerate(texts):
+        if methods is not None:
+            for method in methods:
+                text = method(text)
+        if len(text) > max_content_length:
+            text = text[:max_content_length]
+            texts[i] = text
+    return texts
 
 @retry(stop=stop_after_attempt(5))
-def get_openai_embeddings(inputs, model):
+def get_openai_embeddings(inputs, model=None):
     # make sure each review is a under a certain length
-    for index, input in enumerate(inputs):
-        input = input.replace('\n', ' ')
-        if len(input) > MAX_CONTENT_LENGTH:
-            input = input[:MAX_CONTENT_LENGTH]
-        inputs[index] = input
+    methods = [replace_newline]
+    inputs = preprocess(inputs, 'openai', methods)
 
     response = openai.Embedding.create(
         input=inputs,
@@ -32,9 +46,70 @@ def get_openai_embeddings(inputs, model):
     embeddings = [embed_data['embedding'] for embed_data in response['data']]
     return embeddings
 
-class Dataset():
-    def __init__(self, type, config=None, save=False, split='train'):
+def get_E5_embeddings(inputs, tokenizer=None, model=None, device='cpu'):
+    def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+        last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
+    # For E5 model prefix all inputs with "query: "
+    inputs = [f"query: {input}" for input in inputs]
+    inputs = preprocess(inputs, 'e5')
+
+    model.to(device)
+    model.eval()
+
+    # Tokenize the input texts
+    batch_dict = tokenizer(inputs, max_length=512, padding=True, truncation=True, return_tensors='pt')
+
+    # Move the batch to the device
+    batch_dict = {k: v.to(device) for k, v in batch_dict.items()}
+
+    outputs = model(**batch_dict)
+    embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+    return embeddings
+
+
+class Embedder():
+    def __init__(self, type):
         self.type = type
+        if type == 'openai':
+            self.model = "text-embedding-ada-002"
+            self.tokenizer = None
+            openai.api_key = os.getenv('OPENAI_API_KEY')
+        elif type == 'E5':
+            self.tokenizer = AutoTokenizer.from_pretrained('intfloat/e5-large')
+            self.model = AutoModel.from_pretrained('intfloat/e5-large')
+
+    def embed(self, dataset, batch_size=50, save=False, device='cpu'):
+        if dataset.load(embedder=self.type):
+            return
+
+        reviews = dataset.texts
+        N = len(reviews)
+        num_full_batches = math.ceil(N / batch_size)
+        embeddings = []
+        for index in tqdm(range(num_full_batches)):
+            embeddings_batch = self._get_batch_at(index * batch_size, reviews, batch_size=batch_size, device=device)
+            embeddings += embeddings_batch
+
+        dataset.embeds = np.array(embeddings, dtype=np.float64)
+        if save: dataset.save(self.type)
+
+    def _get_batch_at(self, start, reviews, batch_size=10, device='cpu'):
+        end = start + batch_size
+        if end > len(reviews): end = len(reviews)
+        reviews_subset = reviews[start:end]
+
+        if self.type == 'openai':
+            return get_openai_embeddings(reviews_subset, model=self.model)
+        elif self.type == 'E5':
+            return get_E5_embeddings(reviews_subset, tokenizer=self.tokenizer, model=self.model, device=device)
+
+
+class Dataset():
+    def __init__(self, type, config=None, split='train'):
+        self.type = type
+        self.split = split
         self.dataset_path = config['path']
 
         self.local_path = None
@@ -45,10 +120,6 @@ class Dataset():
         self.embeds = np.float64([])
 
         self.download(split=split)
-        self.create_text_embeddings(split=split)
-
-        if save:
-            self.save(split)
 
     def download(self, split='train'):
         dataset = load_dataset(self.dataset_path, split=split)
@@ -63,37 +134,13 @@ class Dataset():
         self.texts = dataset[text_key]
         self.ratings = np.array(dataset[label_key], dtype=np.uint)
 
-    def create_text_embeddings(self, model="text-embedding-ada-002", batch_size=50,split='train'):
-        # check if embeddings already exist
-        is_loaded = self.load(split=split)
-
-        # if not, create them
-        if not is_loaded:
-            reviews = self.texts
-            N = len(reviews)
-            num_full_batches = math.ceil(N / batch_size)
-            embeddings = []
-            for index in tqdm(range(num_full_batches)):
-                embeddings_batch = self._get_batch_at(index * batch_size, reviews, model=model, batch_size=batch_size)
-                embeddings += embeddings_batch
-
-            self.embeds = np.array(embeddings, dtype=np.float64)
-
-    def _get_batch_at(self, start, reviews, model="text-embedding-ada-002", batch_size=10):
-        end = start + batch_size
-        if end > len(reviews):
-            end = len(reviews)
-
-        reviews_subset = reviews[start:end]
-        return get_openai_embeddings(reviews_subset, model)
-
-    def _get_save_path(self, split):
+    def _get_save_path(self, split, embedder):
         dataset_path = self.dataset_path.replace('/', '_')
-        path = f'embeddings/{self.type}/{dataset_path}/{split}/'
+        path = f'embeddings/{self.type}/{embedder}/{dataset_path}/{split}/'
         return path
 
-    def save(self, split='train'):
-        path = self._get_save_path(split)
+    def save(self, split='train', embedder='openai'):
+        path = self._get_save_path(split, embedder)
 
         # make directory if it doesn't exist
         if not os.path.exists(path):
@@ -109,8 +156,8 @@ class Dataset():
 
         np.save(path + '/ratings.npy', self.ratings)
 
-    def load(self, split='train'):
-        path = self._get_save_path(split)
+    def load(self, split='train', embedder='openai'):
+        path = self._get_save_path(split, embedder)
 
         if (os.path.exists(path + '/embeddings.npy') or os.path.exists(path + '/embeddings.npz')) and os.path.exists(path + '/ratings.npy'):
             if os.path.exists(path + '/embeddings.npz'):
@@ -141,9 +188,9 @@ class MergedDataset():
         self.embeds = np.vstack([dataset.embeds for dataset in self.datasets])
         self.ratings = np.hstack([dataset.ratings for dataset in self.datasets])
 
-    def create_text_embeddings(self, model="text-embedding-ada-002", batch_size=50, split='train'):
-        for dataset in self.datasets:
-            dataset.create_text_embeddings(model, batch_size, split=split)
+    # def create_text_embeddings(self, model="text-embedding-ada-002", batch_size=50, split='train'):
+    #     for dataset in self.datasets:
+    #         dataset.create_text_embeddings(model, batch_size, split=split)
 
     def save(self, split='train'):
         for dataset in self.datasets:
